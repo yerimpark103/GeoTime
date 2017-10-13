fb40628323ff68a23339d46789769164fcc63e1d
yerimpark103 <ypark92@illinois.edu>
2017-10-12 21:48:38 -0500

    First Commit

---
+++ public/js/app.js
@@ -0,0 +1,57 @@
+// Declares the initial angular module "geotime". Module grabs other controllers and services.
+var app = angular.module('geotime', ['ngRoute', 'geotimeServices', 'geotimeControllers', 'geolocation']);
+
+app.config(['$routeProvider','$locationProvider', function($routeProvider, locationProvider) {
+  $routeProvider.
+
+  when('/mapping', {
+    templateUrl: 'partials/mapping.html',
+    controller: 'addCtrl'
+  }).
+  when('/firstview', {
+    templateUrl: 'partials/firstview.html',
+    controller: ''
+  }).
+
+  when('/about', {
+    templateUrl: 'partials/about.html',
+    controller: ''
+  }).
+  when('/visuals', {
+    templateUrl: 'partials/visuals.html',
+    controller: ''
+  }).
+
+  when('/login', {
+      controller: '',
+      templateUrl: ''
+  }).
+  /*
+  when('/details/:id', {
+        templateUrl: 'partials/userdetails.html',
+        controller: 'UserDetailsController'
+    }).
+    when('/adduser', {
+        templateUrl: 'partials/adduser.html',
+        controller: 'AddUserController'
+    }).
+    when('/tasklist', {
+        templateUrl: 'partials/tasklist.html',
+        controller: 'TasksListController'
+    }).
+    when('/addtask', {
+        templateUrl: 'partials/addtask.html',
+        controller: 'AddTaskController'
+    }).
+    when('/taskdetails/:id', {
+        templateUrl: 'partials/taskdetails.html',
+        controller: 'TaskDetailsController'
+    }).
+    when('/edittask/:id', {
+        templateUrl: 'partials/edittask.html',
+        controller: 'EditTaskController'
+    }).*/
+  otherwise({
+    redirectTo: '/firstview'
+  });
+}]);
